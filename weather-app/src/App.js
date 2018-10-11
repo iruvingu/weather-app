8a0f53d9cc1d85655f944d5518714b3e8c8c3582
Irving Flores <rfloresg1002@alumno.ipn.mx>
2018-10-10 19:03:24 -0500

    commit al

--- weather-app/src/App.js
+++ weather-app/src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
+import { Flex, Box } from 'reflexbox'
 import LocationList from './components/LocationList';
 
 const cities = [
@@ -10,19 +11,38 @@ const cities = [
   "Mexico City,mx"
 ]
 
+const Border = props => {
+  console.log(props)
+  return (
+  <Box
+    {...props}
+    style={{
+      ...props.style,
+      boxShadow: 'inset 0 0 0 3px',
+      background: 'red'
+    }}
+  />
+)}
+
 class App extends Component {
 
-  handleSelectionLocation = city => {
-    console.log('handleSelectionLocation')
+  handleSelectedLocation = city => {
+    console.log('handleSelectedLocation')
   }
 
   render() {
     return (
-      <div className="App">
-        <LocationList cities={cities}
-          onSelectedLocation={this.handleSelectionLocation} />
-      </div>
-    );
+      <Flex column>
+        <Border p={1}>1/3</Border>
+        <Box p={1} style={{background: 'blue'}}>1/3</Box>
+        <Box p={1} style={{background: 'green'}}>1/3</Box>
+      
+      </Flex>
+      // <div className="App">
+      //   <LocationList cities={cities}
+      //     onSelectedLocation={this.handleSelectedLocation} />
+      // </div>
+    )
   }
 }
 
